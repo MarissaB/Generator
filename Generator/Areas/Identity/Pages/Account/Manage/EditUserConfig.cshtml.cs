10105678dcabaa273b6ca03d5c2cae392a653047
Marissa <maberresford@comcast.net>
2023-04-12 14:05:34 -0500

    Fixed issue with saving user config

--- Generator/Areas/Identity/Pages/Account/Manage/EditUserConfig.cshtml.cs
+++ Generator/Areas/Identity/Pages/Account/Manage/EditUserConfig.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Generator.Areas.Identity.Data
 {
+    // TODO: Update to Bootstrap 5.3 when it comes out since floating form labels are overlapping with inputs. https://github.com/twbs/bootstrap/issues/37539
     public class EditModel : PageModel
     {
         private readonly Generator.Data.ApplicationDbContext _context;
@@ -26,12 +27,10 @@ namespace Generator.Areas.Identity.Data
         }
 
         [BindProperty]
-        public UserConfig UserConfig { get; set; }
+        public UserConfig UserConfig { get; set; } = default!;
 
         [TempData]
         public string StatusMessage { get; set; }
-
-        private bool AddConfig { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -49,10 +48,6 @@ namespace Generator.Areas.Identity.Data
             if (userconfig != null)
             {
                 UserConfig = userconfig;
-            }
-            else
-            {
-                AddConfig = true;
             }
 
             return Page();
@@ -76,7 +71,7 @@ namespace Generator.Areas.Identity.Data
 
             try
             {
-                var userconfig = await _context.UserConfig.FirstOrDefaultAsync(m => m.UserId == user.Id);
+                var userconfig = await _context.UserConfig.AsNoTracking().FirstOrDefaultAsync(m => m.UserId == user.Id);
                 if (userconfig == null)
                 {
                     _context.UserConfig.Add(UserConfig);
